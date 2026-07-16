7fb338926662b280e9888c828b594ff4710e72a4
Shivam05933 <shivam865593@gmail.com>
2026-07-16 22:46:03 +0530

    going to deploy

--- frontend/src/utils/server.js
+++ frontend/src/utils/server.js
@@ -8,10 +8,23 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://prepai.vercel.app' 
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed'));
+    }
+  },
   credentials: true
 }));
+
+app.set('trust proxy', 1);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,4 +37,8 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
